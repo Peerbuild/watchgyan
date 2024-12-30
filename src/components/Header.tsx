7d272f3a54e46774813162776625abb30642dc9a
Priyobroto Kar <priyobrotokar@gmail.com>
2024-12-30 21:49:50 +0530

    Feat: Add prettier configuration and prettier-plugin-tailwindcss

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,29 @@
-import { playflairDisplay } from "@/lib/fonts";
-import FeatherIcon from "feather-icons-react";
-import Link from "next/link";
-import React from "react";
-import { Button } from "./ui/button";
+import { playflairDisplay } from '@/lib/fonts';
+import FeatherIcon from 'feather-icons-react';
+import Link from 'next/link';
+import React from 'react';
+import { Button } from './ui/button';
 
 const navlinks = [
   {
-    name: "About",
-    link: "#about",
+    name: 'About',
+    link: '#about',
   },
   {
-    name: "Journey",
-    link: "#journey",
+    name: 'Journey',
+    link: '#journey',
   },
   {
-    name: "Newsletter",
-    link: "#newsletter",
+    name: 'Newsletter',
+    link: '#newsletter',
   },
   {
-    name: "blog",
-    link: "/blog",
+    name: 'blog',
+    link: '/blog',
   },
   {
-    name: "Community",
-    link: "/community",
+    name: 'Community',
+    link: '/community',
   },
 ];
 
@@ -50,7 +50,7 @@ const Header = () => {
         </ul>
       </nav>
       <div className="flex gap-8">
-        <Button variant={"ghost"} size={"icon"}>
+        <Button variant={'ghost'} size={'icon'}>
           <FeatherIcon icon="search" />
         </Button>
         <Button className="uppercase">Get in touch</Button>
