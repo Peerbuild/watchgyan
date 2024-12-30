8235e6d7eec4012e915912a0a213806a3bb07c36
Priyobroto Kar <priyobrotokar@gmail.com>
2024-12-30 13:33:42 +0530

    feat: Add header component and basic configurations

---
+++ src/components/Header.tsx
@@ -0,0 +1,62 @@
+import { playflairDisplay } from "@/lib/fonts";
+import FeatherIcon from "feather-icons-react";
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+
+const navlinks = [
+  {
+    name: "About",
+    link: "#about",
+  },
+  {
+    name: "Journey",
+    link: "#journey",
+  },
+  {
+    name: "Newsletter",
+    link: "#newsletter",
+  },
+  {
+    name: "blog",
+    link: "/blog",
+  },
+  {
+    name: "Community",
+    link: "/community",
+  },
+];
+
+const Header = () => {
+  return (
+    <header className="flex items-center justify-between px-12 py-10">
+      <h1 className={`${playflairDisplay.className} text-xl font-medium`}>
+        WatchGyan
+      </h1>
+      <nav>
+        <ul className="flex gap-8">
+          {navlinks.map((link) => {
+            return (
+              <li key={link.name}>
+                <Link
+                  href={link.link}
+                  className="font-medium uppercase text-foreground/60 transition-colors hover:text-foreground"
+                >
+                  {link.name}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+      <div className="flex gap-8">
+        <Button variant={"ghost"} size={"icon"}>
+          <FeatherIcon icon="search" />
+        </Button>
+        <Button className="uppercase">Get in touch</Button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/lib/fonts.ts
@@ -0,0 +1,9 @@
+import { DM_Sans, Playfair_Display } from "next/font/google";
+
+export const playflairDisplay = Playfair_Display({
+  subsets: ["latin"],
+});
+
+export const DMSans = DM_Sans({
+  subsets: ["latin"],
+});
